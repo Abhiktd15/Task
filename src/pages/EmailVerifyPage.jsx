8b7a118ca4fd3c201d91d8f0856315e3c5fd2d33
Abhishek Bhardwaj <ayushindia4@gmail.com>
2024-08-17 15:58:27 +0530

    Add files via upload

---
+++ src/pages/EmailVerifyPage.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar/Navbar.jsx'
+import Footer from '../components/Footer/Footer.jsx'
+import VerifyEmail from '../components/EmailVerify/VerifyEmail.jsx'
+
+const EmailVerifyPage = () => {
+    return (
+        <div>
+            <Navbar/>
+            <VerifyEmail/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default EmailVerifyPage
